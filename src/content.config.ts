4055dc0be703cfc49e0f7734a390be0153883b5c
Kate <kate@Mac.attlocal.net>
2026-03-24 20:29:17 -0700

    Add titleZh to blog collection schema

--- src/content.config.ts
+++ src/content.config.ts
@@ -7,6 +7,7 @@ const blog = defineCollection({
 	schema: () =>
 		z.object({
 			title: z.string(),
+			titleZh: z.string().optional(),
 			description: z.string(),
 			pubDate: z.coerce.date(),
 			updatedDate: z.coerce.date().optional(),
